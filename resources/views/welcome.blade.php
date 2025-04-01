87a1361ff6f1c9d87d7ce8def926f8c9ed8ff68a
ronewa <ronewamavhungu@gmail.com>
2025-04-01 21:15:14 +0200

    removing main tailwind style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,8 @@
             @endif
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex bg-red-500 w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+            <main class="">
+                <p>Mavhungu Ronewa</p>
             </main>
         </div>
 
