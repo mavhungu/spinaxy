69c35c6149392d61cfdc4c20581dbc2da5bb4e60
ronewa <ronewamavhungu@gmail.com>
2025-04-03 21:54:46 +0200

    adding relationships

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -2,15 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Business extends Model
 {
     //
-    protected $fillable = [
-        'bussiness_name',
-        'email',
-        'address',
-        'phone_number'
-    ];
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'bussiness_name', 'email', 'address', 'phone_number'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
