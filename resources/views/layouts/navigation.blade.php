bcdb34d5d705afba2fa3ec1d277e2430f35a77cd
ronewa <ronewamavhungu@gmail.com>
2025-04-03 21:44:44 +0200

    adding router links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,8 +13,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')"> 
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('business.index')" :active="request()->routeIs('business.*')">
+                        {{ __('Businesses') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('booking.index')" :active="request()->routeIs('booking.*')">
+                        {{ __('Bookings') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -70,6 +76,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('business.index')" :active="request()->routeIs('business.*')">
+                {{ __('Businesses') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('booking.index')" :active="request()->routeIs('booking.*')">
+                {{ __('Bookings') }}
             </x-responsive-nav-link>
         </div>
 
